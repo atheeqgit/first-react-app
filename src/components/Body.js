7f82a9832dccea7c4426e874b22e8eb483dbef3e
seyed atheeq <113854491+atheeqgit@users.noreply.github.com>
2023-02-28 23:05:46 +0530

    just uploaded the exisiting files

---
+++ src/components/Body.js
@@ -0,0 +1,29 @@
+function body() {
+  return (
+    <div className="body">
+      <img
+        src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"
+        className="outer-ring"
+      ></img>
+      <div className="img">
+        <img src="https://cdn1.iconfinder.com/data/icons/programing-development-8/24/react_logo-512.png"></img>
+      </div>
+      <ul className="points">
+        <li>
+          react-point gives you fast touch events for your React applications.
+        </li>
+        <li>
+          React is a declarative, efficient, and flexible JavaScript library for
+          building user interfaces.
+        </li>
+        <li>
+          It lets you compose complex UIs from small and isolated pieces of code
+          called “components”.
+        </li>
+        <li>The key feature of React is composition of components.</li>
+      </ul>
+    </div>
+  );
+}
+
+export default body;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+function header() {
+  return (
+    <div className="header">
+      <h2>react-App</h2>
+      <ul>
+        <li>home</li>
+        <li>about</li>
+        <li>contact</li>
+      </ul>
+    </div>
+  );
+}
+
+export default header;
